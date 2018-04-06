f737abac6c039260266d4c2cd47797354943ebde
felanilla <asia.karwan@gmail.com>
2018-04-06 14:03:16 +0200

    parallax effect

--- app/scripts/animation.js
+++ app/scripts/animation.js
@@ -4,6 +4,26 @@ var onPageLoad = utilities.getClasses('on-page-load');
 
 var ANIMATION = {
 
+    parallaxMove: function() {
+
+    $("body").mousemove(function(e) {
+        parallaxIt(e, ".slide", -100);
+        parallaxIt(e, "img", -30);
+    });
+
+    function parallaxIt(e, target, movement) {
+        var $this = $("#oval");
+        var relX = e.pageX - $this.offset().left;
+        var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+        x: (relX - $this.width() / 2) / $this.width() * movement,
+        y: (relY - $this.height() / 2) / $this.height() * movement
+    });
+}
+
+    },
+
     scrollMagic: {
 
         controller: new ScrollMagic.Controller(),

c5761ad64107a65ad255952ccf6b2b05ae5cf4ad
felanilla <asia.karwan@gmail.com>
2018-04-07 22:40:41 +0200

    css changes

--- app/scripts/animation.js
+++ app/scripts/animation.js
@@ -6,21 +6,21 @@ var ANIMATION = {
 
     parallaxMove: function() {
 
-    $("body").mousemove(function(e) {
-        parallaxIt(e, ".slide", -100);
-        parallaxIt(e, "img", -30);
-    });
-
-    function parallaxIt(e, target, movement) {
-        var $this = $("#oval");
-        var relX = e.pageX - $this.offset().left;
-        var relY = e.pageY - $this.offset().top;
-
-    TweenMax.to(target, 1, {
-        x: (relX - $this.width() / 2) / $this.width() * movement,
-        y: (relY - $this.height() / 2) / $this.height() * movement
-    });
-}
+        $("#oval").mousemove(function(e) {
+            parallaxIt(e, ".slide", -100);
+            parallaxIt(e, "img", -30);
+          });
+          
+          function parallaxIt(e, target, movement) {
+            var $this = $("#oval");
+            var relX = e.pageX - $this.offset().left;
+            var relY = e.pageY - $this.offset().top;
+          
+            TweenMax.to(target, 1, {
+              x: (relX - $this.width() / 2) / $this.width() * movement,
+              y: (relY - $this.height() / 2) / $this.height() * movement
+            });
+          }
 
     },
 
